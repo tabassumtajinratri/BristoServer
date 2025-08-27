4fb7a8c60d31335feeaabafc448aed76d3cf4057
tabassumtajinratri <tabassumtajinratri@gmail.com>
2025-08-27 23:42:34 +0600

    update

--- index.js
+++ index.js
@@ -48,6 +48,16 @@ async function run() {
 
     //cart collection
 
+    app.get('/carts',async(req, res)=>{
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+
+
     app.post('/carts', async(req, res)=>{
       const cartItems = req.body
       const result = await cartCollection.insertOne(cartItems)
